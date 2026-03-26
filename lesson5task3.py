bea6db0c7bb4b31d8e2c181437e0d1a4dee615ef
Max <1privetokda@gmail.com>
2026-03-27 01:56:41 +0300

    Add files via upload

---
+++ lesson5task3.py
@@ -0,0 +1,14 @@
+A = int(input())
+B = int(input())
+X = int(input())
+
+if A >= X and B >= X:
+    print(2)
+elif A >= X:
+    print("Mike")
+elif B >= X:
+    print("Ivan")
+elif A + B >= X:
+    print(1)
+else:
+    print(0)
